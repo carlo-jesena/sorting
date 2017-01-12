a98e4d1de82dd8b59598db8ff826fc9b4b641cec
Your Namecarlo <carlo.jesena@gmail.com>
2017-01-12 14:26:17 -0800

    started question 3

--- sorting-interview-questions.js
+++ sorting-interview-questions.js
@@ -1,7 +1,7 @@
 //QUESTION 1
 //Write an O(n) algorithm to sort an array of integers, where you know in advance what the lowest and highest values are.
 //Bucket sort -or- Counting sort?
-// let arr1 = [2, 7, 3, 6, 4, 1]
+let arr1 = [2, 7, 3, 6, 4, 1]
 //create 7 buckets
 //if value of num equals bucket index, put it there
 
@@ -13,17 +13,16 @@ const linearSort = (array, min, max) => {
       buckets[array[i]-min] = array[i];
       }
   // buckets => array with not empty spacces
-  // const clear = (i) => {return i !== ''}
-  // console.log(buckets.filter(clear))
-  console.log('final: ' + buckets)
+  const clear = (i) => {return i !== ''}
+  console.log(buckets.filter(clear))
 }
 
-// linearSort(arr1, 1, 7)
+linearSort(arr1, 1, 7)
 
 //QUESTION 2
 //Write an algorithm to shuffle an array into a random order in-place (i.e. without creating a new array).
 //Math.random with swaps
-let arr1 = [2, 7, 3, 6, 4, 1]
+// let arr1 = [2, 7, 3, 6, 4, 1]
 const shuffle = array => {
   //loop thru array
   for (let i=0; i < array.length; i++) {
@@ -38,8 +37,18 @@ const shuffle = array => {
 console.log(shuffle(arr1))
 console.log(shuffle(arr1))
 console.log(shuffle(arr1))
+
 //QUESTION 3
 //Imagine that I gave you twenty books to sort in alphabetical order. How would you go about it? Can you express this as an algorithm?
-//use Quicksort
-//compare the first letter of each book
-//move the a's to the front, then b's, and so on
+//Group the books by letter
+//Then when you finish that sort, sort each group books alphabetically by the second letter
+
+const sortBooks = array => {
+  //some kind of bucket sort to group books by letter
+
+  //after the bucket sort, write algorithm to sort books my second(third, or fourth) letter in the title
+
+  //place the sorted books on the shelf (return array)
+
+
+}
