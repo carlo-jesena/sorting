46aa3e071fdaa5b63e9789984e32f61c225cdca8
Your Namecarlo <carlo.jesena@gmail.com>
2017-01-12 09:11:41 -0800

    rewrite bubble sort

--- sorting.js
+++ sorting.js
@@ -1,1 +1,16 @@
 // bubble sort
+const swap = (array, i, j) => {
+  let temp = array[i]
+  array[i] = array[j]
+  array[j] = temp
+}
+const bubbleSort = array => {
+  let swaps = 0
+  for (let i=0,i<array.length-1; i++) {
+    if (array[j] > array[i]) {
+      swap(array, i, j)
+      swaps++
+    }
+  }
+  if (swaps>0) array
+}
