592fde2a6e5a340cc5ad36e5a874b7b17951e6f1
trig0 <allengordon0@gmail.com>
2017-01-12 11:34:34 -0600

    bubble and merge working

--- sorting.js
+++ sorting.js
@@ -1,16 +1,53 @@
 // bubble sort
 const swap = (array, i, j) => {
-  let temp = array[i]
-  array[i] = array[j]
-  array[j] = temp
+    let temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
 }
 const bubbleSort = array => {
-  let swaps = 0
-  for (let i=0,i<array.length-1; i++) {
-    if (array[j] > array[i]) {
-      swap(array, i, j)
-      swaps++
+    let swaps = 0
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            swap(array, i, i + 1)
+            swaps++
+        }
     }
-  }
-  if (swaps>0) array
+    if (swaps > 0) {
+      return bubbleSort(array)
+    }
+    return array
 }
+
+const mergeSort = array => {
+    if (array.length <= 1) {
+      return array;
+    }
+    let middle = Math.floor(array.length / 2);
+    let left = array.slice(0, middle);
+    let right = array.slice(middle, array.length);
+    left = mergeSort(left);
+    right = mergeSort(right);
+    return merge(left, right, array);
+}
+
+const merge = (left, right, array) => {
+    let leftIndex = 0
+    let rightIndex = 0
+    let outputIndex = 0
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            array[outputIndex++] = left[leftIndex++]
+        } else { array[outputIndex++] = right[rightIndex++] }
+    }
+    for (let i = leftIndex; i < leftIndex.length; leftIndex++) {
+      array[outputIndex++] = left[i]
+    }
+    for (let i = rightIndex; i < rightIndex.length; rightIndex++) {
+      array[outputIndex++] = right[i]
+    }
+    return array
+}
+
+array1 = [1,5,8,3,4,2,7,6]
+console.log(bubbleSort(array1))
+console.log(mergeSort(array1))
